278231f3709b7f9eecc951c6772d1118dd8584ea
Sadman Sakib <101496701+ssakib4040@users.noreply.github.com>
2022-07-12 16:55:41 +0600

    margin typo

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,43 +1,100 @@
-import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite - Updated 4x</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="container-fluid">
+      <div className="mb-3">
+        <h2>CDN</h2>
+        <div className="bg-gray-1 p-1">
+          https://cdn.jsdelivr.net/gh/ssakib4040/CSS-Helicopter/css/style.min.css
+        </div>
+      </div>
+
+      <div className="mb-3">
+        <h2>Colors</h2>
+        <div className="text-red">text-red</div>
+        <div className="text-green">text-green</div>
+        <div className="text-blue">text-blue</div>
+        <div className="text-yellow">text-yellow</div>
+        <div className="text-black">text-black</div>
+        <div className="text-white">text-white</div>
+        <div className="text-orange">text-orange</div>
+        <div className="text-brown">text-brown</div>
+
+        <div className="text-gray-1">text-gray-1</div>
+        <div className="text-gray-2">text-gray-2</div>
+        <div className="text-gray-3">text-gray-3</div>
+        <div className="text-gray-4">text-gray-4</div>
+      </div>
+
+      <div className="mb-3">
+        <h2>Backgrounds</h2>
+        <div className="mb-1 bg-red">bg-red</div>
+        <div className="mb-1 bg-green">bg-green</div>
+        <div className="mb-1 bg-blue">bg-blue</div>
+        <div className="mb-1 bg-yellow">bg-yellow</div>
+        <div className="mb-1 bg-black">bg-black</div>
+        <div className="mb-1 bg-white">bg-white</div>
+        <div className="mb-1 bg-orange">bg-orange</div>
+        <div className="mb-1 bg-brown">bg-brown</div>
+
+        <div className="bg-gray-1">bg-gray-1</div>
+        <div className="bg-gray-2">bg-gray-2</div>
+        <div className="bg-gray-3">bg-gray-3</div>
+        <div className="bg-gray-4">bg-gray-4</div>
+      </div>
+
+      <div className="mb-3">
+        <h2>Borders</h2>
+        <div className="border-red">border-red</div>
+        <div className="border-green">border-green</div>
+        <div className="border-blue">border-blue</div>
+        <div className="border-yellow">border-yellow</div>
+        <div className="border-black">border-black</div>
+        <div className="border-white">border-white</div>
+        <div className="border-orange">border-orange</div>
+        <div className="border-brown">border-brown</div>
+      </div>
+
+      <div className="mb-3">
+        <h2>Buttons</h2>
+        <button className="button-red">button-red</button>
+        <button className="button-green">button-green</button>
+        <button className="button-blue">button-blue</button>
+        <button className="button-yellow">button-yellow</button>
+        <button className="button-black">button-black</button>
+        <button className="button-white">button-white</button>
+        <button className="button-orange">button-orange</button>
+        <button className="button-brown">button-brown</button>
+      </div>
+
+      <div className="mb-3">
+        <h2>Links</h2>
+        <a href="#" className="link-red">
+          link-red
+        </a>
+        <a href="#" className="link-green">
+          link-green
+        </a>
+        <a href="#" className="link-blue">
+          link-blue
+        </a>
+        <a href="#" className="link-yellow">
+          link-yellow
+        </a>
+        <a href="#" className="link-black">
+          link-black
+        </a>
+        <a href="#" className="link-white">
+          link-white
+        </a>
+        <a href="#" className="link-orange">
+          link-orange
+        </a>
+        <a href="#" className="link-brown">
+          link-brown
+        </a>
+      </div>
     </div>
   );
 }
